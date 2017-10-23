0eb639e2dc3f0198e33c303070232a3fe02f0b80
HMBSbige <admin@bige0.com>
2017-10-23 20:05:25 +0800

    委托

--- Network-programming-training/Form1.Designer.cs
+++ Network-programming-training/Form1.Designer.cs
@@ -32,12 +32,18 @@
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,6 +55,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBox1
             // 
@@ -56,32 +63,72 @@
             this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Location = new System.Drawing.Point(35, 23);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(572, 207);
+            this.groupBox1.Size = new System.Drawing.Size(572, 242);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
             // groupBox3
             // 
-            this.groupBox3.Location = new System.Drawing.Point(298, 20);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(305, 32);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(261, 163);
+            this.groupBox3.Size = new System.Drawing.Size(261, 204);
             this.groupBox3.TabIndex = 1;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "groupBox3";
             // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(13, 41);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(242, 147);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
             // groupBox2
             // 
-            this.groupBox2.Location = new System.Drawing.Point(21, 20);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(17, 32);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(261, 163);
+            this.groupBox2.Size = new System.Drawing.Size(261, 204);
             this.groupBox2.TabIndex = 0;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 41);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(242, 147);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(35, 247);
+            this.textBox1.Location = new System.Drawing.Point(35, 271);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(572, 21);
             this.textBox1.TabIndex = 2;
@@ -129,7 +176,12 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.groupBox4.ResumeLayout(false);
             this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
@@ -147,6 +199,10 @@
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Network-programming-training/Form1.cs
+++ Network-programming-training/Form1.cs
@@ -16,5 +16,68 @@ namespace Network_programming_training
         {
             InitializeComponent();
         }
+
+        private delegate void WriteToTextBox(string str);
+
+        private WriteToTextBox _writeToTextBox;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1 != null && checkBox1.Checked)
+            {
+                if (label1 != null)
+                {
+                    label1.Text = @"运行中......";
+                    label1.Refresh();
+                }
+                textBox2?.Clear();
+                _writeToTextBox=WriteTextBox1;
+                WriteText(_writeToTextBox);
+                if (label1 != null)
+                    label1.Text = @"任务1完成";
+            }
+            if (checkBox2 != null && checkBox2.Checked)
+            {
+                if (label2 != null)
+                {
+                    label2.Text = @"运行中......";
+                    label2.Refresh();
+                }
+                textBox3?.Clear();
+                _writeToTextBox = WriteTextBox2;
+                WriteText(_writeToTextBox);
+                if (label2 != null)
+                    label2.Text = @"任务2完成";
+            }
+        }
+        private void WriteText(WriteToTextBox writeMethod)
+        {
+            var textBox = this.textBox1;
+            if (textBox != null)
+            {
+                string strData = textBox.Text;
+                writeMethod?.Invoke(strData);
+            }
+        }
+        private void WriteTextBox1(string strTxt)
+        {
+            var textBox = this.textBox2;
+            if (textBox != null)
+                textBox.Text = strTxt;
+        }
+
+        private void WriteTextBox2(string strTxt)
+        {
+            var textBox = this.textBox3;
+            if (textBox != null)
+                textBox.Text = strTxt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //设置文本框获取焦点
+            this.ActiveControl = this.textBox1;
+            //this.txt_Input.Focus();
+        }
     }
 }
